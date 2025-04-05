7818979731a0b49bea87002ed5d867b0557acd21
VitorCoelhoNeto <vitordiogo2000@sapo.pt>
2025-04-06 00:47:38 +0100

    Added mouse raycast

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private const float STOPPING_DISTANCE = .1f;
+
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, targetPosition) > STOPPING_DISTANCE)
+        {
+            Vector3 moveDirection = (this.targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+
+        if(Input.GetKey(KeyCode.T))
+        {
+            Move(new Vector3(4,0,4));
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
