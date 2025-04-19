9b1c26a9437a6e6d71a0a2048756354156c46244
VitorCoelhoNeto <vitordiogo2000@sapo.pt>
2025-04-19 23:41:41 +0100

    Added walk and idle animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    private const string IS_WALKING= "IsWalking";
+
+    [SerializeField] private Animator unitAnimator;
+
     private const float STOPPING_DISTANCE = .1f;
 
     private Vector3 targetPosition;
+    private float rotateSpeed = 7f;
 
     private void Update()
     {
+        
         if(Vector3.Distance(transform.position, targetPosition) > STOPPING_DISTANCE)
         {
             Vector3 moveDirection = (this.targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+            transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            unitAnimator.SetBool(IS_WALKING, true);
+        }
+        else
+        {
+            unitAnimator.SetBool(IS_WALKING, false);
         }
 
         if(Input.GetMouseButtonDown(0))
