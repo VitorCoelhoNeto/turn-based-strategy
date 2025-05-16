c9240f22b1a149751c4824b2566de24c2b8cd85f
VitorCoelhoNeto <vitordiogo2000@sapo.pt>
2025-05-16 01:29:25 +0100

    Added basic camera controls (move and rotation)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float moveSpeed = 10f;
+    private float rotationSpeed = 100f;
+
+    private void Update()
+    {
+        // Basic WASD movement controls
+        Vector3 inputMoveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.z += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.z -= 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x += 1f;
+        }
+
+        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+
+        // Rotation controls
+        Vector3 rotationVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y -= 1f;
+        }
+
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+}
